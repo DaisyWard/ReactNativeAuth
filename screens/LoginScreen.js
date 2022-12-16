f9b8289942db6ee7b806e5df5ae95a40a21bfbd0
DaisyyWard <daisy.w@mmtdigital.co.uk>
2022-12-16 12:03:22 +0000

    Add authentication

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,33 @@
-import AuthContent from '../components/Auth/AuthContent';
+import AuthContent from '../components/Auth/AuthContent'
+import { useState, useContext } from 'react'
+import { login } from '../util/auth'
+import LoadingOverlay from '../components/ui/LoadingOverlay'
+import { Alert } from 'react-native'
+import { AuthContext } from '../store/auth-context'
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const authCtx = useContext(AuthContext)
+
+  const loginHandler = async ({ email, password }) => {
+    setIsAuthenticating(true)
+    try {
+      const token = await login(email, password)
+      authCtx.authenticate(token)
+    } catch (error) {
+      Alert.alert(
+        'Authentication failed',
+        'Could not log you in, please check your credentials'
+      )
+      setIsAuthenticating(false)
+    }
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message='Logging you in...' />
+  }
+  return <AuthContent isLogin onAuthenticate={loginHandler} />
 }
 
-export default LoginScreen;
+export default LoginScreen
